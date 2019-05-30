88e3b4e238cb3e88e44f8061b8d153eb9802e594
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-30 03:07:36 +0000

    Deploy matheusdc/ifood-frontend-test to github.com/matheusdc/ifood-frontend-test.git:gh-pages

---
+++ precache-manifest.1de2ee90b396e3677c1dd74d3e8c8e90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dea58a4c7495a345813fa3e7ccc34e36",
+    "url": "/ifood-frontend-test/index.html"
+  },
+  {
+    "revision": "a03062f01e5f0d0cc493",
+    "url": "/ifood-frontend-test/static/css/2.fd4afd7e.chunk.css"
+  },
+  {
+    "revision": "2aafbda52c94bb20689c",
+    "url": "/ifood-frontend-test/static/css/main.c1efd232.chunk.css"
+  },
+  {
+    "revision": "a03062f01e5f0d0cc493",
+    "url": "/ifood-frontend-test/static/js/2.b1cc1f9d.chunk.js"
+  },
+  {
+    "revision": "2aafbda52c94bb20689c",
+    "url": "/ifood-frontend-test/static/js/main.16cbe50a.chunk.js"
+  },
+  {
+    "revision": "03fb1823aa76949644b5",
+    "url": "/ifood-frontend-test/static/js/runtime~main.1301fedf.js"
+  },
+  {
+    "revision": "31bad8d4fc4413d54f8c19930d3739b9",
+    "url": "/ifood-frontend-test/static/media/spotifyLogo.31bad8d4.png"
+  }
+]);
